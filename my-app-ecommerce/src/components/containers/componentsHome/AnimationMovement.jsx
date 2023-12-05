c437523b06779480678a945ca76716ab2903df58
=snyder <=>
2023-12-05 16:13:46 -0500

    add component AnimationMovemevent to the page home

---
+++ my-app-ecommerce/src/components/containers/componentsHome/AnimationMovement.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+function AnimationMovement({backgroundColor}) {
+    
+    const refContainer = useRef()
+    
+    const animateMovement = ()=>{
+        
+        const randomMovementX = Math.floor(Math.random() * refContainer.current.parentElement.clientWidth)
+        const randomMovementY = Math.floor(Math.random() * refContainer.current.parentElement.clientHeight)
+        
+        refContainer.current.style.transform = `translateX(${randomMovementX}%) translateY(${randomMovementY}%)`
+       
+
+       
+        
+    }
+
+
+
+    useEffect(()=> {
+       
+       const idAnimation =  setInterval(()=>{
+            animateMovement()
+        } , 9000)
+
+        return ()=> clearInterval(idAnimation)
+    }, [])
+  return (
+
+        <div ref={refContainer} className={`w-[80px]  ${backgroundColor} transition-transform duration-[9s]  ease-linear h-[80px] rounded-full absolute`}></div>
+    
+  )
+}
+
+export default AnimationMovement
--- my-app-ecommerce/src/containers/Home.jsx
+++ my-app-ecommerce/src/containers/Home.jsx
@@ -1,14 +1,20 @@
 import ScrollAnimationContainer from "../components/ScrollAnimationContainer"
+import AnimationMovement from "../components/containers/componentsHome/AnimationMovement"
 import Banner from "../components/containers/componentsHome/Banner"
 import SectionCategoryProducts from "../components/containers/componentsHome/SectionCategoryProducts"
 
 function Home() {
   return (
-    <div className="w-screen relative h-screen">
-      
-       <div className="absolute bg-blue-800 right-0 border w-[500px] rounded-tl-full h-full">
+    <div className="w-screen relative h-screen border border-black">
+      <h1>añadir iconos de descuento entre otros en las animaciones de <AnimationMovement></AnimationMovement></h1>
+      <AnimationMovement backgroundColor={"bg-yellow-600"} />
+      <AnimationMovement backgroundColor={"bg-purple-600"} />
+      <AnimationMovement backgroundColor={"bg-green-600"}/>
+      <AnimationMovement />
+       <div className="absolute bg-blue-300 right-0 border w-[500px] rounded-tl-full h-[90%] bottom-0">
       
        </div>
+       {/* <img className="absolute right-0 w-[500px]" src="https://img.freepik.com/vector-gratis/descuentos-venta-temporada-presenta-compra-visita-boutiques-compras-lujo-cupones-promocionales-reduccion-precio-ofertas-especiales-vacaciones-ilustracion-metafora-concepto-aislado-vector_335657-2766.jpg?size=626&ext=jpg" alt="" /> */}
         
     </div>
   )
