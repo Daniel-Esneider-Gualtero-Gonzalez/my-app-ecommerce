9445f03ff04c85f8c496ae5bcd4320a705b7ffc2
Daniel Gualtero <esneidergonzales2020@gmail.com>
2023-12-11 20:47:00 -0500

    filter catecorias changes and change ui

--- my-app-ecommerce/src/components/containers/productlistpage/ProductFilters.jsx
+++ my-app-ecommerce/src/components/containers/productlistpage/ProductFilters.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react"
+import useProductCategories from "../../../hooks/useProductCategories"
 
 
 function ProductFilters() {
+  const {categories,getCategories} = useProductCategories()
+  const [category,setCategory] = useState(null)
+
+  const handleChangeFilterCategory = (e)=>{
+
+    if(e.target.checked){
+      setCategory(e.target.value)
+    }else setCategory(null)
+
+  }
+
+  useEffect(()=> {
+    getCategories()
+  },[])
   return (
     <>
       Products Filter
-      <div className="flex border ">
+      <div className="flex border-black border-b  p-1">
         <h1>Price:</h1>
-        <input onChange={(e)=>{console.log("filter product price", e.target.value)}} className="ml-5" max={10} type="range" />
+        <input onChange={(e) => { console.log("filter product price", e.target.value) }} className="ml-5" max={10} type="range" />
       </div>
-      <span>Category</span>
-      <div>
-        <input type="checkbox" name="" id="" />
+
+      <div className="mt-6 border-b border-black p-1">
+        <span>Category</span>
+        <div>
+          {categories ? categories.map(cate=>{
+            return <div><input onChange={handleChangeFilterCategory} type="checkbox"  checked={category === cate} value={cate}  id={cate} /><label htmlFor={cate}  className="ml-1 capitalize font-semibold">{cate}</label></div>
+          }): null}
+
+          {category}
+        </div>
       </div>
     </>
   )
--- my-app-ecommerce/src/components/productSearchBar/ProductSearchBar.jsx
+++ my-app-ecommerce/src/components/productSearchBar/ProductSearchBar.jsx
@@ -14,8 +14,8 @@ function ProductSearchBar() {
 
   }
   return (
-    <form onSubmit={handleSearchProducts}  className=" flex items-center bg-white rounded p-1 ">
-      <input placeholder="Search Products" name="searchProduct" className='outline-none  border-b border-violet-600 pl-2 py-1   ' type="text" />
+    <form onSubmit={handleSearchProducts}  className=" flex w-[300px] items-center bg-white rounded p-1 ">
+      <input placeholder="Search Products" name="searchProduct" className='md:w-full outline-none  border-b border-violet-600 pl-2 py-1   ' type="text" />
       <button className=" text-violet-600 flex h-full ">
         <CiSearch  className="object-cover text-2xl"/>
       </button>
