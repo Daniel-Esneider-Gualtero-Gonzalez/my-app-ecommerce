9758ed5ab56dc0458cd4fca39f4fab4e49652173
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-01-24 12:15:11 -0500

    changes ui and responsive desing grid

--- my-app-ecommerce/src/components/NavBar.jsx
+++ my-app-ecommerce/src/components/NavBar.jsx
@@ -8,63 +8,74 @@ import BtnRegister from "./buttons/BtnRegister"
 import { GrMenu } from "react-icons/gr";
 
 function NavBar() {
-    const [showsBarUser,setShowBarUser] = useState(false)
+    const [showsBarUser, setShowBarUser] = useState(false)
     const refScroll = useRef(0)
-    const [stylesNavScroll,setStylesNavScroll] = useState(null)
+    const [stylesNavScroll, setStylesNavScroll] = useState(null)
 
-    useEffect(()=> {
-        window.document.addEventListener("scroll",()=>{
-            if(window.scrollY === 0) return setStylesNavScroll(null)
-            if(window.scrollY < refScroll.current){
+    useEffect(() => {
+        window.document.addEventListener("scroll", () => {
+            if (window.scrollY === 0) return setStylesNavScroll(null)
+            if (window.scrollY < refScroll.current) {
                 setStylesNavScroll(
-                    {'position':'sticky','top':'0', 'zIndex': '999'}
+                    { 'position': 'sticky', 'top': '0', 'zIndex': '999' }
                 )
-            }else{
+            } else {
                 setStylesNavScroll(null)
             }
 
             refScroll.current = window.scrollY
 
-            
+
         })
-    } ,[])
-    
+    }, [])
+
     return (
-       <nav style={stylesNavScroll} className="w-screen bg-blue-300 py-2 px-4 flex justify-between sm:grid sm:grid-cols-4 sm:gap-1">
-          
+        <nav style={stylesNavScroll} className=" grid grid-cols-3 lg:grid-cols-4 p-4  bg-blue-300 py-2 border border-black">
 
-            <div className=''>
-                <img className='w-[80px] h-full ' src="https://cdn.pixabay.com/photo/2023/08/29/19/42/goose-8222013_1280.jpg" alt="" />
+
+            <div className='flex lg:cdjustify-center items-center border'>
+                <button className='p-1 lg:hidden  w-fit h-fit rounded flex justify-center items-center bg-white mt-1 border  text-2xl ' onClick={() => setShowBarUser(true)}>
+                    <GrMenu className=" text-[30px]" />
+                </button>
+                <img className=' hidden lg:block h-[40px] w-[60px]   md:rounded-none  ' src="https://cdn.pixabay.com/photo/2023/08/29/19/42/goose-8222013_1280.jpg" alt="" />
             </div>
-           
 
-            <div className=' py-5 hidden md:block'>
-                <NavItems />
-           </div>
-           <div className="flex items-center">
-            <ProductSearchBar />
-           </div>
 
-            
-            <div className='  flex justify-end items-center h-full   '>
-                
+            <div className=' border flex justify-center p-1 items-center'>
+                <ul className=" hidden lg:block lg:flex">
+                    <NavItems />
+                </ul>
+
+                <img className=' lg:hidden rounded-full h-[50px] w-[50px]   md:rounded-none  ' src="https://cdn.pixabay.com/photo/2023/08/29/19/42/goose-8222013_1280.jpg" alt="" />
+
+            </div>
+
+            <div className="flex  justify-center border p-1 items-center">
+                <ProductSearchBar />
+            </div>
+
+
+            <div className='border hidden lg:block lg:flex gap-1'>
+
+
                 <BtnLogin />
                 <BtnRegister />
-                
-                <button className='p-2 flex justify-center items-center bg-white animationBurbuje border mx-1 text-2xl ' onClick={()=>setShowBarUser(true)}>
-                <GrMenu  className=" text-[30px]"/>
+
+
+                <button className='p-1 w-full  md:w-fit md:h-fit rounded flex justify-center items-center bg-white mt-1 border  text-2xl ' onClick={() => setShowBarUser(true)}>
+                    <GrMenu className=" text-[30px]" />
                 </button>
-                {showsBarUser ? <SideBarUserProfile showBarSideUser={setShowBarUser} />  : null}
+
             </div>
 
 
-           
+            {showsBarUser ? <SideBarUserProfile  showBarSideUser={setShowBarUser} /> : null}
 
-           
-           
-           
 
-       </nav>
+
+
+
+        </nav>
     )
 }
 
