4f4ec49867f35a22fe3a5f190cca292204ac4b38
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-01-24 12:15:54 -0500

    cahnges elements tags

--- my-app-ecommerce/src/components/NavItems.jsx
+++ my-app-ecommerce/src/components/NavItems.jsx
@@ -1,24 +1,25 @@
 import { NavLink } from "react-router-dom"
 import { useContextAuth } from "../contexts/contextAuth"
 
-function NavItems({className}) {
+function NavItems() {
 
-    const {user} = useContextAuth()
+    const { user } = useContextAuth()
     return (
-        <ul className={className ? className : 'flex w-fit  mx-auto '}>
 
+        <>
             {/* PUBLIC NAV ITEMS */}
             <li>
                 <NavLink to={"/"} >Home</NavLink>
             </li>
             <li>
-                <NavLink  to={"/products"}>Products</NavLink>
+                <NavLink to={"/products"}>Products</NavLink>
             </li>
             <li>
                 <NavLink to={"/aboutas"}>About as</NavLink>
             </li>
 
-        </ul>
+        </>
+
 
     )
 }
