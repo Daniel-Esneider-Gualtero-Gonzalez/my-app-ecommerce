5d8d4863f2b268a168bed7fb786b652377147780
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-02-19 20:10:22 -0500

    change footer ui and info

--- my-app-ecommerce/src/components/Footer.jsx
+++ my-app-ecommerce/src/components/Footer.jsx
@@ -7,11 +7,24 @@ function Footer() {
 
   return (
     <>
-      <footer  className="  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]  from-green-300 via-blue-500 to-purple-600">
+      <footer className="border flex  flex-col gap-1 rounded lg p-1 md:flex-row md: justify-between">
         
-                 
-
-                
+          <span >© 2024 <a href="https://flowbite.com/" >My ecommerce</a>. Casi todos los derechos reservados.</span>
+          <ul className="flex flex-wrap items-center  text-sm ">
+            <li>
+              <a href="#" >Products</a>
+            </li>
+            <li>
+              <a href="#" >Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" >Licensing</a>
+            </li>
+            <li>
+              <a href="#" >Contact </a>
+            </li>
+          </ul>
+   
       </footer>
 
     </>
