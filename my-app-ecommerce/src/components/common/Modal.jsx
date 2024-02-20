4aedd97ce142386f3c0c7006131cbc4072c8fe52
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-02-19 20:16:24 -0500

    Improve Logic and semantic html

--- my-app-ecommerce/src/components/common/Modal.jsx
+++ my-app-ecommerce/src/components/common/Modal.jsx
@@ -1,28 +1,35 @@
+import { MdCancel } from "react-icons/md";
 
+const modal = {
+    id: "",
+    title: "Titulo del modal",
+    status: false,
+    callbackCancel: () => { },
+    callbackAcept: () => { }
 
-function Modal({title="Title Modal por defauldt", statusModal=false,closeModal=Function,accept = Function, cancel=closeModal, isForLoading = false, children}) {
+}
 
+function Modal({ id=modal.id, title = modal.title, status = modal.status, callbackCancel = modal.callbackCancel , callbackAcept = modal.callbackAcept, children }) {
+
+    if (!status) return null
     
-    if(!statusModal) return null
-    
-
 
     return (
-        <dialog className='modal activeModal   bg-white  z-50 fixed flex flex-col mx-auto top-[20%] sm:top-[25%]  p-2  left-0 right-0    w-[340px] sm:w-[500px] h-[340px] md:w-[600px] md:h-[400px] border border-black'>
-            <div className=' border flex  justify-end py-3'> <button  onClick={()=> closeModal()} className='w-[60px] h-[40px]  hover:bg-red-600 hover:border-none border-red-600 bg-transition justify-center items-center rounded text-2xl border '>🧩</button> </div>
-            <h1 className='  whitespace-nowrap text-ellipsis overflow-hidden h-10 text-center  text-sm font-semibold mt-3'>{title}</h1>
+        <dialog  id={id} className='modal  shadow-lg gap-1  p-2 activeModal  z-50 fixed flex flex-col mx-auto my-auto border rounded-lg     left-0 right-0 bottom-0 top-0    w-[340px] sm:w-[500px] h-[340px] md:w-[600px] md:h-[400px] '>
+            <div className="h-fit flex  items-center justify-between">
+            <h1 className=' whitespace-nowrap text-ellipsis overflow-hidden  text-center  text-sm font-semibold '>{title}</h1>
+            <button className="border rounded" onClick={() => callbackCancel()} > <MdCancel /> </button>
+            </div>
 
 
+            <article className='relative  overflow-auto'>{children}</article>
 
+            <div className=' flex gap-1 self-end items-center mt-auto '>
 
-            <section className=' mt-1 border relative   overflow-auto'>{children}</section>
+                <button onClick={(e) => callbackAcept()} className='border  flex justify-center items-center  rounded border-green-600 bg-transition hover:bg-green-600 hover:text-white '>Aceptar</button>
+                <button onClick={() => callbackCancel()} className='border flex justify-center items-center  rounded border-red-600 bg-transition hover:bg-red-600 hover:text-white '>Cancelar</button>
 
-            <div className='justify-center flex items-end mt-auto border  py-2 '>
-                    
-                    <button onClick={()=> accept()} className='border mx-2 flex justify-center items-center w-[100px] h-[40px] rounded border-green-600 bg-transition hover:bg-green-600 hover:text-white '>Aceptar</button>
-                    <button onClick={()=>  cancel()} className='border flex justify-center items-center w-[100px] h-[40px] rounded border-red-600 bg-transition hover:bg-red-600 hover:text-white '>Cancelar</button>
-                   
-                </div>
+            </div>
 
 
 
