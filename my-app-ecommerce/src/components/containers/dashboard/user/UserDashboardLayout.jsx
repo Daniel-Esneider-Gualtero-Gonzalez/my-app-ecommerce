9159288279f6136522bb70bb233712d469370186
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-02-20 12:56:22 -0500

    I'll use it later

---
+++ my-app-ecommerce/src/components/containers/dashboard/user/UserDashboardLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserDashboardLayout() {
+  return (
+    <section className='border'>UserDashboardLayout</section>
+  )
+}
+
+export default UserDashboardLayout
