df9def730e7bcfaec23a1dfa4a63aa44e5324f54
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-02-27 21:10:16 -0500

    change ui product cart

--- my-app-ecommerce/src/components/containers/products/ProductCard.jsx
+++ my-app-ecommerce/src/components/containers/products/ProductCard.jsx
@@ -3,35 +3,32 @@ import RemoveCartItemButton from "../../buttons/RemoveCartItemButton";
 import { useState } from "react";
 
 const product = {
-    title: "title Product",
+    title: "Title Product",
     description: "Aquí ira un breve texto de la descripcion de este producto",
     price: 100,
     image: "https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 }
 
 
-function ProductCard({ title = product.title, description = product.description, image = product.image, price = product.price, onAddProduct = () => {}, onDeleteProduct = () => { }, isInCart = false }) {
-    
+function ProductCard({ title = product.title, description = product.description, image = product.image, price = product.price, onAddProduct = () => { }, onDeleteProduct = () => { }, isInCart = false }) {
+
 
 
     return (
         <>
-            <article className="w-full flex flex-col gap-2 rounded-lg border p-[0.3rem] ">
-                <img className="h-[200px] w-full min-w-full max-w-full  rounded-tl-lg rounded-tr-lg " src={image} alt="product" />
-                <div className="flex justify-between">
-                    <h2 className="  font-bold  max-w-[60%]  overflow-hidden  text-ellipsis whitespace-nowrap text-gray-500">{title}</h2>
-                    <span className="">${price}</span>
+
+
+            <article className="w-full flex flex-col gap-1 max-w-sm p-1 border  rounded-lg shadow ">
+
+                <img className="w-full mx-auto rounded-t-lg h-[200px]" src={image} alt="product image" />
+
+
+                <h5 className="text-md h-20 overflow-hidden text-ellipsis font-semibold tracking-tight ">{title}</h5>
+
+                <div className="flex items-center justify-between">
+                    <span className="text-xl font-semibold">${price}</span>
+                    <button  href="#" className="border rounded p-2"><FaCartShopping /></button>
                 </div>
-
-                <p className=" max-w-full h-[100px]  overflow-hidden  text-ellipsis  font-semibold text-gray-600">{description}</p>
-                <div className="flex  justify-end  ">
-
-                    {isInCart === false && <button onClick={onAddProduct} className='flex place-content-center place-items-center border rounded-full  hover:bg-green-600 hover:text-white  p-2  '>
-                        <FaCartShopping />
-                    </button>}
-                    {isInCart && <RemoveCartItemButton onClick={onDeleteProduct} className={'flex place-content-center place-items-center border rounded-full p-2'} />}
-                </div>
-
 
             </article>
 
